654367921602dba923daabf127e3d91a7f0660fe
couchbasebrian <brian.williams@couchbase.com>
2016-05-20 13:15:21 -0700

    added one

--- memcache-log-count-dcp.sh
+++ memcache-log-count-dcp.sh
@@ -19,24 +19,26 @@ then
   SDDC=`grep dcp $FILE | grep "Shutting down dcp connections" | wc -l`
   ATA=`grep dcp $FILE | grep "Attempting to add" | wc -l`
   STC=`grep dcp $FILE | grep "Stream closing" | wc -l`
+  STCR0=`grep dcp $FILE | grep "Stream created with start seqno 0 and" | wc -l`
   #
   VBU=`grep dcp $FILE | grep vb | cut -f7- -d":" | cut -f2- -d"(" | cut -f1 -d")" | sort -u | wc -l`
   #
-  echo "Total lines in this log file          :  $LINES"
-  echo "Total dcp lines                       :  $DCP"
-  echo "Total Stream Created!                 :  $STREAMCREATED"
-  echo "Total Create Stream                   :  $CREATESTREAM"
-  echo "Total Stream closing                  :  $STC"
-  echo "Total Backfill                        :  $BACKFILL"
-  echo "Total dead state                      :  $DEADSTATE"
-  echo "Total Attempting to add               :  $ATA"
-  echo "Total Sending disk snapshot           :  $SENDINGDS"
-  echo "Total Create Takeover Stream          :  $CREATETOSTREAM"
-  echo "Total Vbucket marked as dead          :  $VBD"
-  echo "Total Conn flow control buffer        :  $CFCB"
-  echo "Total Received response               :  $RR"
-  echo "Total Shutting down dcp connections   :  $SDDC"
-  ACCOUNTEDFOR=$((BACKFILL + CREATESTREAM + STREAMCREATED + CREATETOSTREAM + SENDINGDS + DEADSTATE + CFCB + RR + VBD + SDDC + ATA + STC))
+  echo "Total lines in this log file                :  $LINES"
+  echo "Total dcp lines                             :  $DCP"
+  echo "Total Stream Created!                       :  $STREAMCREATED"
+  echo "Total Create Stream                         :  $CREATESTREAM"
+  echo "Total Stream closing                        :  $STC"
+  echo "Total Backfill                              :  $BACKFILL"
+  echo "Total dead state                            :  $DEADSTATE"
+  echo "Total Attempting to add                     :  $ATA"
+  echo "Total Sending disk snapshot                 :  $SENDINGDS"
+  echo "Total Create Takeover Stream                :  $CREATETOSTREAM"
+  echo "Total Vbucket marked as dead                :  $VBD"
+  echo "Total Conn flow control buffer              :  $CFCB"
+  echo "Total Received response                     :  $RR"
+  echo "Total Shutting down dcp connections         :  $SDDC"
+  echo "Total Stream created with start seqno 0 and :  $STCR0"
+  ACCOUNTEDFOR=$((BACKFILL + CREATESTREAM + STREAMCREATED + CREATETOSTREAM + SENDINGDS + DEADSTATE + CFCB + RR + VBD + SDDC + ATA + STC + STCR0))
   UNACCOUNTED=$((DCP - ACCOUNTEDFOR))
   echo "Total Accounted for                   :  $ACCOUNTEDFOR"
   echo "Total Unaccounted for                 :  $UNACCOUNTED"
